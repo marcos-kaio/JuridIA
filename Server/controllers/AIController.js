6b71d11fc767e2b703064badb13c9127fe7bad6c
marcos-kaio <marcosb1@outlook.com>
2025-06-08 19:54:11 -0300

    geração do PDF funcionando normalmente

--- Server/controllers/AIController.js
+++ Server/controllers/AIController.js
@@ -5,16 +5,15 @@ export default async function AIControler(req, res) {
   try {
     const pdfBuffer = req.file.buffer; // recebe arquivo do buffer
     const simplifiedText = await callGemini(pdfBuffer); // envia pdf para a API
-    
-    // const outPdfBuffer = await generatePdf(simplifiedText); // gera pdf a partir da resposta
-    // console.log('Tamanho do PDF gerado:', outPdfBuffer.length);
+    const outPdfBuffer = await generatePdf(simplifiedText); // transforma texto simplificado em pdf
 
+    // retorna documento simplificado no formato pdf
     res
-      .header("Content-Type", "text")
-      //.header("Content-Disposition", 'attachment; filename="simplificado.pdf"')
-      .send(simplifiedText); // retornando por enquanto em formato de texto
+      .header("Content-Type", "application/pdf")
+      .header("Content-Disposition", 'attachment; filename="simplificado.pdf"')
+      .send(outPdfBuffer);
   } catch (err) {
-    console.error("AIController error: ", err);
-    res.status(500).json({ error: "erro ao simplificar o documento." });
+    console.error("simplifyController error:", err);
+    res.status(500).json({ error: "Erro ao simplificar o documento." });
   }
 }
