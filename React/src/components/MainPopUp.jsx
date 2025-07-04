a8a3910ac1482223a47984714c8b1305054c24eb
miguelsndc <mglsndc@gmail.com>
2025-07-03 21:29:48 -0300

    migrar o popup

--- React/src/components/MainPopUp.jsx
+++ React/src/components/MainPopUp.jsx
@@ -1,10 +1,16 @@
-import './MainPopUp.css';
-
 function MainPopUp({ isOpen, onClose, children }) {
   if (!isOpen) return null; // caso não esteja aberto, retorna nada
   return (
-    <div className='overlay' onClick={onClose}>
-      <div className='popup' onClick={(e) => e.stopPropagation()}>
+    <div
+      className='fixed top-0 left-0 w-dvw h-dvh bg-black/50 flex items-center justify-center z-10'
+      onClick={onClose}
+    >
+      <div
+        className='min-w-72 bg-blue-950 p-5 shadow-lg z-100 border 
+          rounded-lg w-2/5 h-80 flex flex-col items-center justify-center
+        '
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>
