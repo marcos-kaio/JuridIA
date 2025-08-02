52c47bb6db50cd104c41b009cefccaae9e483c39
MariaFFA <mariaffa2003@gmail.com>
2025-08-02 11:03:35 -0300

    bugfix: garantir que as fontes Poppins e Montserrat sejam reconhecidas pelo Tailwind

--- React/tailwind.config.js
+++ React/tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'montserrat': ['Montserrat', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
